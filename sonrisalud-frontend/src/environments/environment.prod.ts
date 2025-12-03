e4f374e382cab2f89ba9ee172d85d894c0c637c1
Jenciso20 <jencisoquispe@gmail.com>
2025-12-02 23:11:34 -0500

    baseurl agregado

--- sonrisalud-frontend/src/environments/environment.prod.ts
+++ sonrisalud-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://sonrisalud-1xc4-rcnpn5tpn-jenciso20s-projects.vercel.app/api'
+  apiBaseUrl: 'https://sonrisalud-1xc4.vercel.app/api'
   // apiBaseUrl: 'http://localhost:3000/api'
 };
