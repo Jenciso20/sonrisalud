ea25e400bfee4b1c63e167854303f93fb99551ec
Jenciso20 <jencisoquispe@gmail.com>
2025-12-02 22:52:03 -0500

    deploy

--- sonrisalud-frontend/src/environments/environment.prod.ts
+++ sonrisalud-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://sonrisalud-cnkz-ortmoh4y6-jenciso20s-projects.vercel.app/api'
+  apiBaseUrl: 'https://sonrisalud-1xc4-rcnpn5tpn-jenciso20s-projects.vercel.app/api'
+  // apiBaseUrl: 'http://localhost:3000/api'
 };
